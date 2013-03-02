d3860ba81c3a18ad8f26e3466861f58909181700
ruilov <ruilov@gmail.com>
2013-03-02 17:36:03 -0500

    first bet fair test

--- driver.rb
+++ driver.rb
@@ -2,38 +2,49 @@ require "rubygems"
 require "net/http"
 require "uri"
 require 'nokogiri'
+require "./net.rb"
 
-uri = URI.parse("http://www.betfair.com/exchange/football/event?id=26967072")
-# puts uri.path
-http = Net::HTTP.new(uri.host)
-res = http.get(uri.path)
-puts res
+while(true) do
+  time = Time.new.strftime("%Y-%m-%d %H:%M:%S")
+  
+  begin
+    uri = URI.parse("http://www.betfair.com/exchange/football/event?id=26967072")
+    path = Net::make_path(uri)
+    http = Net::HTTP.new(uri.host)
+    res = http.get(path)
+    doc = Nokogiri::HTML(res.body)
+    
+    markets = doc.xpath("//em[@class=\"market-title\"]")
+    markets.each do |market| 
+      if(market.content.strip!="Match Odds") then next end;
+      odds_table = market.parent.parent.next.next.xpath(".//table[@class=\"runner-table\"]")[0]
+      rows = odds_table.xpath(".//tr[@class]")
+      
+      rows.each do |row|
+        cols = row.xpath(".//td")
+        name = cols[0].xpath(".//div[@class=\"runner-name\"]//span")[0].content
+        
+        back_price = cols[1].xpath(".//span[@class=\"price\"]")[0].content.strip
+        lay_price = cols[2].xpath(".//span[@class=\"price\"]")[0].content.strip
+        puts "#{time} | #{name} | #{back_price} | #{lay_price}"
+      end
+      break  
+    end
+  rescue Exception => e
+    puts "Error at #{time}"
+    puts e
+    puts e.backtrace.inspect
+  end
+  
+  sleep(60) # in seconds
+end
 
-# uri = URI.parse("http://www.anp.gov.br/preco/prc/Resumo_Semanal_Tipologia.asp")
-# 
-# (700..710).each do |week|
-  # res = Net::HTTP.post_form(uri, {
-      # # "selSemana" => "710*de 20/01/2013 a 26/01/2013",
-      # "selSemana" => "#{week}*",
-      # # "desc_Semana" => "de 20/01/2013 a 26/01/2013",
-      # # "cod_Semana" => "710",
-      # "tipo" => "2",
-      # "rdResumo" => "4",
-      # "selEstado" => "AC*ACRE",
-      # "selCombustivel" => "487*Gasolina"
-  # })
-#   
-  # print "#{week}|"
-  # doc = Nokogiri::HTML(res.body)
-  # rows = doc.xpath("//table//tr")
-  # (3...(rows.length)).each do |ri|
-    # row = rows[ri]
-    # cols = row.xpath(".//td")
-#     
-    # city = cols[0].content
-    # consumer_avg = cols[3].content.gsub(",",".").to_f
-    # distrib_avg = cols[8].content.gsub(",",".").to_f
-    # print "#{consumer_avg}|#{distrib_avg}|"
-  # end
-  # puts ""
-# end
+# file = File.new('./testpage.html','w')
+# file.write(res.body)
+# file.close
+  
+# file = File.new("./testpage.html","r")
+# doc = ""
+# while( line = file.gets); doc += line end
+# doc = Nokogiri::HTML(doc)
+# puts doc
